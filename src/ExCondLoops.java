5081f8a02e4156455ad1d59ebafc2455b13aaebe
emilianviorel <buduganemil@yahoo.com>
2023-10-31 22:20:44 +0200

    Java ex conditional, loops,

---
+++ src/ExCondLoops.java
@@ -0,0 +1,38 @@
+public class ExCondLoops {
+    public static void main(String[] args) {
+        //Exercise 1
+        /*
+        Write a program to display all integers from 0 to 10 (inclusive).
+         */
+        System.out.println("---Exercise 1---");
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+        //Exercise 2
+        /*
+        Write a program to display the integers from 20 to 10 (inclusive) in descending order.
+         */
+        System.out.println("---Exercise 2---");
+
+        for (int i = 20; i >= 10; i--) {
+            System.out.println(i);
+        }
+
+        //Exercise 3
+        /*
+        Write a program to display all positive even numbers up to and including 100
+         */
+        System.out.println("---Exercise 3---");
+
+        for (int i = 0; i <= 100; i += 2) {
+            System.out.println(i);
+        }
+
+        //Exercise 4
+        System.out.println("---Exercise 4---");
+        //Exercise 5
+        System.out.println("---Exercise 5---");
+    }
+}
---
+++ src/ExOptCondInstr.java
@@ -0,0 +1,93 @@
+public class ExOptCondInstr {
+    public static void main(String[] args) {
+        //Exercise 1
+        /*
+        Declare a variable of type int,with the name "temperature" and give it a value of your choice.
+        If its value is lower than 18, display "too cold".
+        If the value is between 18 and 22, display "ok".If the value is above 22, display "too hot".
+        */
+        System.out.println("---Exercise 1---");
+
+        int temperature = 18;
+
+        if (temperature < 18) {
+            System.out.println("Is too cold!");
+        } else if (temperature >= 18 && temperature <= 22) {
+            System.out.println("Is comfy.");
+        } else System.out.println("Is too hot!");
+
+        //Exercise 2
+        /*
+        Two variables are given:
+        char gender (gen) – initialize with a value of your choice (possible values "m" or "f")
+        boolean married - initialize with a value of your choice
+        Write a program using conditional statements, so that:
+            - for gender "m", to display "Mr", regardless of whether the person is married or not
+            - for gender "f" to display "Miss", if she is not married or "Mrs", if she is married
+         */
+        System.out.println("---Exercise 2---");
+
+        char gender = 'm';
+        boolean married = false;
+
+        if (gender == 'm') {
+            System.out.println("Mr.");
+        } else if (gender == 'f' && married == false) {
+            System.out.println("Miss.");
+        } else System.out.println("Mrs.");
+
+        //Exercise 3
+        /*
+        Declare and initialize two variables of type int.
+        Write a program that displays the variable with the larger value of the two: "[x] is
+        greater than [y]", where x and y are the values of the variables.
+        If the two are equal, the program must display "are equal".
+         */
+        System.out.println("---Exercise 3---");
+
+        int x = 70;
+        int y = 100;
+
+        if (x < y) {
+            System.out.printf("Number %d is greater than number %d. %n", y, x);
+        } else if (x > y) {
+            System.out.printf("Number %d is greater than number %d. %n", x, y);
+        } else System.out.println("Numbers are equal");
+
+        //Exercise 4
+        /*
+        Declare a third variable.
+        Write a program to display the variable with the highest value among the 3.
+         */
+        System.out.println("---Exercise 4---");
+
+        int z = 50;
+        int highestValue;
+
+        if (x >= y && x >= z) {
+            highestValue = x;
+        } else if (y >= x && y >= z) {
+            highestValue = y;
+        } else highestValue = z;
+        System.out.printf("Highest value is %d %n", highestValue);
+
+        //Exercise 5
+        /*
+        Declare a char type variable with the name letter and give it a value of your choice.
+        If the value is a consonant, display “[letter] is a consonant”, if it is a vowel, display "[letter]
+        is vowel", where letter is the value of the variable.
+         */
+        System.out.println("---Exercise 5---");
+
+        char letter = 'o';
+
+        switch (letter) {
+            case 'a', 'e', 'i', 'o', 'u':
+                System.out.printf("%s is a vowel! %n", letter);
+                break;
+            default:
+                System.out.printf("%s is a consonant! %n", letter);
+        }
+
+    }
+}
